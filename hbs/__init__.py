c428c446acb94cddc0daf1fe66ea0ab499fbe00b
Chance <chenranlin.17@gmail.com>
2025-02-21 13:34:09 +0800

    更新版本号至0.3.1，添加utils、boundary、mesh和conformal_welding模块的导入

--- hbs/__init__.py
+++ hbs/__init__.py
@@ -4,3 +4,7 @@ A Python library for complex analysis and conformal mapping computations.
 """
 
 from .hbs import get_hbs, reconstruct_from_hbs
+from . import utils
+from . import boundary
+from . import mesh
+from . import conformal_welding
